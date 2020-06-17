c9cafe84210eaede9959bf73ab36865fefc90350
VoNgocAnhKhoa18It2 <anhkhoadn20@gmail.com>
2020-06-17 12:43:36 +0700

    add lich hoc phan

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,10 +3,46 @@ var connection = require('../connection.js');
 module.exports = {
 
     select: (req, res) => {
-
-        connection.query("SELECT * FROM `"+req.params.table+"`", (err, rows) => {
+        connection.query("SELECT * FROM `" + req.params.table + "`", (err, rows) => {
             if (err) throw err;
             res.send(rows);
+        })
+    },
+
+    select_addSV: (req, res) => {
+        var sql = "SELECT `sinh_vien`.*,`lop`.`ten_lop` FROM `sinh_vien`,`lop` WHERE sinh_vien.ma_lop = lop.id AND `msv` NOT IN(SELECT `ma_sinh_vien` FROM `ct_lop_hp`,`lop_hoc_phan` WHERE ct_lop_hp.`ma_lop_hp`= lop_hoc_phan.ma_lop_hp AND lop_hoc_phan.ma_mon_hoc = '"+req.params.ma_mon_hoc+"')";
+        connection.query(sql, (err, rows) => {
+            if (err) throw err;
+            res.send(rows);
+        })
+    },
+
+    insert_addSV: (req, res) => {
+        var sql = "INSERT INTO `ct_lop_hp`(`ma_lop_hp`, `ma_sinh_vien`) VALUES ";
+        console.log(req.body);
+        var listmsv = req.body.msv.split(",");
+        var s = [];
+        var s2 = [];
+        for (var i = 0; i < listmsv.length;i++) {
+            s.push("('"+req.body.ma_lop_hp+"','"+listmsv[i]+"')");
+            s2.push("('"+req.body.mmh+"','"+listmsv[i]+"')");
+        }
+        connection.query(sql+s.toString(),(err, rows) => {
+            if (err) throw err;
+            var sql_diem = "INSERT INTO `diem_hp`(`ma_mon_hoc`, `ma_sinh_vien`) VALUES "
+            connection.query(sql_diem+s2.toString(),(error, results) => {
+                if (error) throw error;
+                res.send(true);
+            })
+        })
+    },
+
+    editSV_lhp: (req, res) => {
+        console.log(req.body);
+        var sql = "UPDATE `ct_lop_hp` SET `ma_lop_hp`='"+req.body.ma_lop_hp+"' WHERE `id`= "+ req.body.id;
+        connection.query(sql, (err, rows) => {
+            if (err) throw err;
+            res.send(true);
         })
     },
 
@@ -215,9 +251,70 @@ module.exports = {
             req.body.bat_dau,
             req.body.ket_thuc,
         ];
-        connection.query(sql_account,Subject, (err, rows) => {
+        connection.query(sql_account, Subject, (err, rows) => {
             if (err) throw err;
             res.redirect("/home/subject");
+        })
+    },
+
+    lop_hoc_phan: (req, res) => {
+        var sql = "SELECT `lop_hoc_phan`.*,`mon_hoc`.`ten_mon_hoc` FROM  `lop_hoc_phan`,`mon_hoc` where mon_hoc.`ma_mon_hoc` = lop_hoc_phan.`ma_mon_hoc`";
+        connection.query(sql, (err, rows) => {
+            if (err) throw err;
+            var sql = "SELECT * FROM `mon_hoc`";
+            connection.query(sql, (err, subject) => {
+                if (err) throw err;
+                res.render('page/classStudy', {
+                    title: 'Modules',
+                    user: res.locals.user,
+                    classStudies: rows,
+                    subjects: subject
+                })
+            })
+        })
+    },
+
+    add_lop_hp: (req, res) => {
+        var lop_hoc_phan = [
+            req.body.ma_lop_hp,
+            req.body.ten_lop_hp,
+            req.body.mon_hoc
+        ];
+        var sql = "INSERT INTO `lop_hoc_phan`(`ma_lop_hp`, `ten_lop_hp`, `ma_mon_hoc`) VALUES (?,?,?)";
+        connection.query(sql, lop_hoc_phan, (err, rows) => {
+            if (err) throw err;
+            res.redirect("/home/modules");
+        })
+    },
+
+    update_lop_hp: (req, res) => {
+        var sql = "UPDATE lop_hoc_phan SET ? WHERE id =" + req.body.id;
+        connection.query(sql, req.body, (error, results, fields) => {
+            if (error) throw error;
+            res.redirect("/home/modules");
+        })
+    },
+
+    show_lop_hp: (req, res) => {
+        var sql = "SELECT ct_lop_hp.*,sinh_vien.ten,lop.ten_lop FROM `lop_hoc_phan`,`ct_lop_hp`,`sinh_vien`,`lop` "
+        +"WHERE lop_hoc_phan.ma_lop_hp = ct_lop_hp.ma_lop_hp AND ct_lop_hp.ma_sinh_vien = sinh_vien.msv AND sinh_vien.ma_lop = lop.id AND ct_lop_hp.ma_lop_hp='"+req.params.lop+"'";
+        connection.query(sql, (error, results) => {
+            if (error) throw error;
+            var sql_lhp = "SELECT * FROM `lop_hoc_phan` where ma_lop_hp='"+req.params.lop+"'";
+            connection.query(sql_lhp, (err, rows) => {
+                if (err) throw err;
+                var sql_allclass = "SELECT * FROM `lop_hoc_phan` where ma_mon_hoc='"+rows[0].ma_mon_hoc+"'";
+                connection.query(sql_allclass, (err, lhp) => {
+                    if (err) throw err;
+                    res.render('page/listStudentmodules', {
+                        title: 'Module | '+rows[0].ten_lop_hp,
+                        user: res.locals.user,
+                        liststudent: results,
+                        lop_hp: rows,
+                        list_lhp: lhp
+                    })
+                })
+            })
         })
     },
 
@@ -225,11 +322,24 @@ module.exports = {
         var sql = "SELECT * FROM `giao_vien` AS gv,`lich_hoc` as lh,`lop_hoc_phan` as lhp where lh.`ma_lop_hp` = lhp.`ma_lop_hp` AND lh.mgv = gv.mgv;";
         connection.query(sql, (err, rows) => {
             if (err) throw err;
-            res.render('page/calendar', {
-                title: 'Calendar',
-                user: res.locals.user,
-                calenders: rows,
+            var sql_mh = "SELECT * FROM `mon_hoc`"
+            connection.query(sql_mh, (err, mon_hoc) => {
+                if (err) throw err;
+                res.render('page/calendar', {
+                    title: 'Calendar',
+                    user: res.locals.user,
+                    calenders: rows,
+                    subjects : mon_hoc
+                })
             })
+        })
+    },
+
+    lhp_calendar: (req, res) => {
+        var sql = "SELECT * FROM `lop_hoc_phan` WHERE ma_mon_hoc='"+req.params.id+"'";
+        connection.query(sql, (err, rows) => {
+            if (err) throw err;
+            res.send(rows);
         })
     },
 }
